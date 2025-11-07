509dc15c4c86b456b6a053189860cf973cf592bf
Kadson Pedro <kadson.pedro@gmail.com>
2025-11-07 15:54:57 -0400

    chore: tratar html-docx como pacote externo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@prisma/adapter-libsql", "@libsql/client"]
+    serverComponentsExternalPackages: ["@prisma/adapter-libsql", "@libsql/client", "html-docx-js"]
   }
 };
 
