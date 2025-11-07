e0ec9e095675727ac848c89709662285b409a409
Kadson Pedro <kadson.pedro@gmail.com>
2025-11-07 17:12:09 -0400

    fix: defaultViewport opcional

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -23,7 +23,7 @@ export async function renderDocumentPdf({ html, header, footer }: PdfPayload): P
   const launchOptions: PuppeteerLaunchOptions = isServerless
     ? {
         args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
+        defaultViewport: chromium.defaultViewport ?? undefined,
         executablePath: await chromium.executablePath(),
         headless: chromium.headless
       }
