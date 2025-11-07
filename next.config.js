ec77b120062b71b87f5e372ca813b04708c31d88
Kadson Pedro <kadson.pedro@gmail.com>
2025-11-07 18:56:54 -0400

    chore: marcar puppeteer-core como pacote externo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@prisma/adapter-libsql", "@libsql/client", "html-docx-js"]
+    serverComponentsExternalPackages: ["@prisma/adapter-libsql", "@libsql/client", "html-docx-js", "puppeteer-core"]
   }
 };
 
